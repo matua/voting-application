cffd4ce5f003167038b71e6a71310f69df3ce137
matua <$9x=4cg^zNNbF&H.UQR_vmN8jSb2U95>
2021-05-11 10:04:04 +0200

    init setup

---
+++ src/main/java/com/matuageorge/votingapplication/VotingApplication.java
@@ -0,0 +1,12 @@
+package com.matuageorge.votingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingApplication.class, args);
+    }
+}
---
+++ src/test/java/com/matuageorge/votingapplication/VotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matuageorge.votingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
